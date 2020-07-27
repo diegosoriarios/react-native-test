dfe5df0d493c2b892ac135936b7ab87ab60ee809
diegosoriarios <diegosoriarios@gmail.com>
2020-07-27 10:03:32 -0300

    Image Picker

--- mobileTest/src/components/ImageChoose/index.js
+++ mobileTest/src/components/ImageChoose/index.js
@@ -1,10 +1,39 @@
 import React from 'react';
-import { TouchableHighlight, Image } from 'react-native';
+import { TouchableHighlight, Image, Platform } from 'react-native';
 import styles from './styles'
+import ImagePicker from 'react-native-image-picker'
 
 const ImageChoose = ({ image, onChangeImage }) => {
+
+  const chooseImage = () => {
+    let options = {
+      title: 'Select Image',
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    
+    ImagePicker.launchImageLibrary(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      } else {
+        const responseUri = Platform.OS === "android" ? response.uri : response.uri.replace('file://', '')
+        const source = { uri: responseUri };
+
+      onChangeImage(source.uri)
+    }
+    });
+  }
+
   return (
-    <TouchableHighlight onPress={onChangeImage}>
+    <TouchableHighlight onPress={chooseImage}>
       <Image style={styles.image} source={{ uri: image }} />
     </TouchableHighlight>
   );
